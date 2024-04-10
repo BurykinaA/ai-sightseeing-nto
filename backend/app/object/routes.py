e8f42c4bc528e2e5628407b171bcbf88b96b5b9d
Alina Burykina <alina4268@mail.ru>
2024-04-10 10:56:50 +0300

    change paths

--- backend/app/object/routes.py
+++ backend/app/object/routes.py
@@ -9,23 +9,35 @@ from flask_cors import cross_origin
 # import io
 # import cv2
 
-from app.queries.queries import get_object_info
+from app.queries.queries import get_object_info, get_filtered_objects_info
 from app.connection.sqllite3_connection import Sqlite3Connection
 
 
 db_connection = Sqlite3Connection()
 
 
+@cross_origin()
+@object.get("/api/object")
+def get_object_by_id(id):
+    try:
+        object_info = get_object_info(db_connection, id)
+        return make_response(object_info, 200)
+    except Exception as e:
+        print(e)
+        return make_response({"error": "Internal Server Error"}, 500)
+    
 
 @cross_origin()
 @object.get("/api/object")
-def make_correction():
+def get_objects():
     try:
-        object_info = get_object_info(db_connection, 'W38411380')
-        
-        return make_response(object_info, 200)
+        city = request.args.get('city')
+        kind = request.args.get('category')
+        rate = request.args.get('rate')
 
+        object_info = get_filtered_objects_info(db_connection, city, kind, rate)
+    
+        return make_response(object_info, 200)
     except Exception as e:
         print(e)
-        return make_response({"error": "no_body"})
-
+        return make_response({"error": "Internal Server Error"}, 500)
--- backend/app/queries/queries.py
+++ backend/app/queries/queries.py
@@ -11,3 +11,41 @@ def get_object_info(db, object_id):
     WHERE o.id = '{safe_object_id}'
     """
     return db.get(query)
+
+
+def get_filtered_objects_info(db, city=None, kind=None, rate=None):
+    where_clauses = []
+    
+    # Для безопасности используйте параметризованные запросы вместо форматирования строк
+    params = {}
+    
+    if city:
+        city = city.replace("'", "''")  # Лучше использовать параметризованные запросы
+        where_clauses.append("o.city = :city")
+        params["city"] = city
+        
+    if rate:
+        kind = kind.replace("'", "''")  # И здесь тоже
+        where_clauses.append("k.type = :kind")
+        params["kind"] = kind
+
+    if kind:
+        rate = rate.replace("'", "''")  # И здесь тоже
+        where_clauses.append("o.rate = :rate")
+        params["rate"] = rate
+
+    # Формируем базовый запрос
+    query = """
+    SELECT o.id, o.name, o.city, o.lon, o.lat, o.rate, o.description, p.base64, k.type
+    FROM object o
+    LEFT JOIN obj_photo op ON o.id = op.id_obj
+    LEFT JOIN photo p ON op.id_photo = p.id
+    LEFT JOIN obj_kind ok ON o.id = ok.id_obj
+    LEFT JOIN kind k ON ok.id_kind = k.id
+    """
+    
+    # Добавляем условия WHERE, если они есть
+    if where_clauses:
+        query += " WHERE " + " AND ".join(where_clauses)
+    
+    return db.get(query, params)
