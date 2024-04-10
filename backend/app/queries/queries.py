44ec12fd1f2b34d9cf87f10f1d110be201ec2fc3
Alina Burykina <alina4268@mail.ru>
2024-04-11 00:35:26 +0300

    fix bag

--- backend/app/queries/queries.py
+++ backend/app/queries/queries.py
@@ -83,7 +83,7 @@ def get_filtered_objects_info(city=None, kind=None, rate=None, limit=1, offset=1
         where_clauses.append(f"({kind_conditions})")
 
     if rate:
-        rate_conditions = " OR ".join([f"o.rate = {r}" for r in rate])
+        rate_conditions = " OR ".join([f"o.rate = '{r}'" for r in rate])
         where_clauses.append(f"({rate_conditions})")
 
     query = """
