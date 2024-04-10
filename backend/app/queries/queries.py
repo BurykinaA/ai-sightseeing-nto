937e79fbbc8570e27dab8a99d5274904673adce1
Alina Burykina <alina4268@mail.ru>
2024-04-10 22:36:17 +0300

    fix queries

--- backend/app/queries/queries.py
+++ backend/app/queries/queries.py
@@ -44,7 +44,7 @@ def get_filtered_objects_info(city=None, kind=None, rate=None, limit=1, offset=1
         where_clauses.append(f"o.city = '{city}'")
 
     if kind:
-        kind_conditions = " OR ".join([f"'{k}' IN o.kind" for k in kind])
+        kind_conditions = 'or '.join([f"o.kind like '%{k}%' " for k in kind])
         where_clauses.append(f"({kind_conditions})")
 
     if rate:
@@ -71,8 +71,6 @@ def get_filtered_objects_info(city=None, kind=None, rate=None, limit=1, offset=1
     query += f" LIMIT {limit} OFFSET {offset}"
 
     db = Sqlite3Connection()
-
-    print(query)
 
     result = db.get(query)
     formatted_result = [
