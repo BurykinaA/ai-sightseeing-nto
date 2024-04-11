e68c50493508bb80cddf7bb5c6e931960f5d79eb
kokosikEH <borisenkoen2002@gmail.com>
2024-04-11 11:48:47 +0300

    add city to text research

--- front/src/components/TextSearch.jsx
+++ front/src/components/TextSearch.jsx
@@ -8,13 +8,49 @@ import FileCard from './FileCard';
 import { modalTheme } from '../theme';
 import { URL } from '../const';
 import { ObjContext } from '../context';
+import SelectDD from './SelectDD';
 
 
 function TextSearch({objID, disabled, setObjects}) {
   const {obj, setObj}= useContext(ObjContext)
   const [openModal, setOpenModal] = useState(false);
-  const [data, setData]=  useState({text:''})
+  const [data, setData]=  useState({text:'', city:''})
+  const handleFilterChangeNew = (data, type) => {
+    var dataa ={[type]: data}
+    
+    setData(prevState => ({
+      ...prevState,
+      ...dataa
+    }));
+  };
+  const stat={
+    "owner": {
+        "values": [
+            {
+                "id": "vlad",
+                "value": "Владимир",
+                "coordinates": [56.129042, 40.407215],
+            },
+            {
+                "id": "yar",
+                "value": "Ярославль",
+                "coordinates": [57.626074, 39.884470]
 
+            },
+            {
+                "id":  "nn",
+                "value": "Нижний Новгород",
+                "coordinates": [56.326887, 44.005986]
+            },
+            {
+                "id": "ekb",
+                "value": "Екатеринбург",
+                "coordinates": [56.838011, 60.597465]
+            },
+            
+        ]
+    },
+   }
 
     
     const handleSubmit=(event)=>{
@@ -44,26 +80,21 @@ function TextSearch({objID, disabled, setObjects}) {
         <div className="w-[564px] h-px justify-center items-center inline-flex">
               <div className="w-[564px] h-px bg-indigo-300 rounded-[1px]" />
           </div>
+          <div>
+                Город*
+                <SelectDD absolute={true} oneAnsw={true} opened={false}  filter={data.city}  handleFilterChangeNew={handleFilterChangeNew} name='Город' type='city' value={stat&&stat.owner.values} />
+              </div>
           <form onSubmit={handleSubmit} className='flex flex-col gap-[16px]'>
           
 
-            <div className=''>
+            <div className='mt-4'>
                 <p className="font-normal text-[#0B1F33] mb-2" > Описание</p>
                 <input required className=" bg-[#F5F7FA] rounded-lg h-10 w-full outline-0 px-2 py-2.5" placeholder="Описание" value={data.text} onChange={(e)=>setData({...data, text:e.target.value})}/>
             </div>
-             <div>
+             <div className='mt-4'>
              <button 
               type='reset' 
-              onClick={() => (setOpenModal(false), setData({
-                subject_id:parseInt(objID, 10),
-                last_name:'',
-                first_name:'',
-                middle_name:'',
-                phone_number:'',
-                email:'',
-                contact_type:'',
-                comment:'',
-              }))} 
+              onClick={() => (setOpenModal(false), setData({text:'', city:''}))} 
               className='mr-[16px] blueButtonInherit'
               placeholder='pup'
             >
@@ -74,6 +105,7 @@ function TextSearch({objID, disabled, setObjects}) {
               // onClick={() => setOpenModal(false)} 
               className='blueButton'
               placeholder='pup'
+              disabled={data.city==''||data.text==''}
             >
               Поиск
             </button>
