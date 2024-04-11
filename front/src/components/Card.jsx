ce201df71cbf0df0b15e3d2ff79505a7e5774312
kokosikEH <borisenkoen2002@gmail.com>
2024-04-11 11:53:41 +0300

    label&score

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -9,10 +9,12 @@ function Card({item}) {
   return (
     
       <div className='min-w-[420px]  w-[420px]  bg-white shadow-md rounded-xl flex gap-2  flex-col items-center p-4 hover:scale-[102%] duration-200 hover:-translate-y-0.5 '>
-        <img className='w-[380px] min-h-[260px] max-h-[260px] rounded-lg object-cover ' src={item.url||'data:image/jpg;base64,'+item.photo}/>
+       {(item.url||item.photo)&& <img className='w-[380px] min-h-[260px] max-h-[260px] rounded-lg object-cover ' src={item.url||'data:image/jpg;base64,'+item.photo}/>}
        <div className='h-full flex flex-col items-start gap-2'>
         <p className='text-xl text-left font-semibold'>{item.name}</p>
         <p className=' text-left'>{item.description}</p>
+        <p className='text-3xl'>{item.label}</p>
+        <p className='text-3xl'>{item.score}</p>
        
         <div className='flex items-center gap-2'>
         <b className='font-semibold'> Важность достопримечательности: </b><Rate item={item.rate}/>
